7ba40c5af658c4f6c1e8a49c66f9180b7f813af4
Tara Routray <dev@tararoutray.com>
2025-03-31 21:18:01 +1000

    finish

--- todo.py
+++ todo.py
@@ -1,8 +1,13 @@
+import random
+
 HELP = """
 help - напечатать справку по программе.
 add - добавить задачу в список (название задачи запрашиваем у пользователя)
 show - напечатать все добавленные задачи.
-exit - выход"""
+exit - выход
+random - добавлять случайную задачу на дату Сегодня"""
+
+RANDOM_TASKS = ["Записаться на курс в Нетологию", "Написать Гвидо письмо", "Покормить кошку", "Помыть машину"]
 
 tasks = {
 
@@ -10,25 +15,36 @@ tasks = {
 
 run = True
 
+def add_todo(date, task):
+    if date in tasks:
+        # Дата есть в словаре
+        # Добавляем задачу в список
+        tasks[date].append(task)
+    else:
+        # Даты нет в словаре
+        # Создаем запись с ключем date
+        tasks[date] = [task]
+    print("Задача", task, "добавлена на дату", date)
+
 while run:
     command = input("Введите команду: ")
 
     if command == "help":
         print(HELP)
     elif command == "show":
-        print(tasks)
+        date = input("Введите дату для отображения списка задач: ")
+        if date in tasks:
+            for task in tasks[date]:
+                print('-',task)
+        else:
+            print("Такой даты нет")
     elif command == "add":
         date = input("Введите дату выполнения задачи: ")
         task = input("Введите название задачи: ")
-        if date in tasks:
-            #Дата есть в словаре
-            #Добавляем задачу в список
-            tasks[date].append(task)
-        else:
-            #Даты нет в словаре
-            #Создаем запись с ключем date
-            tasks[date] = [task]
-        print("Задача", task, "добавлена на дату", date)
+        add_todo(date, task)
+    elif command == "random":
+        task = random.choice(RANDOM_TASKS)
+        add_todo("Сегодня", task)
     elif command == "exit":
         print("Спасибо за использование! До свидания!")
         break
